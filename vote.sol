25f427a60447dee28e7517504cdff028eb09d8f4
ligang21 <ligang880128@gmail.com>
2021-07-13 08:01:54 +0800

    add authorizeTo and delegate

--- vote.sol
+++ vote.sol
@@ -14,9 +14,39 @@ contract Vote{
     }
 
     Proposal[] public Proposals;
-    address public creator;
+    address public Creator;
+    mapping(address => Voter) public Voters;
 
     constructor(bytes32[] memory ProposalNames){
-        creator = msg.sender;
+        Creator = msg.sender;
+        Voters[msg.sender].weight = 1;
+        // todo:
+        // remove the duplicate item from ProposalNames
+        for(uint i=0; i<ProposalNames.length; i++){
+            Proposals.push(Proposal({
+                name: ProposalNames[i],
+                count: 0
+            }));
+        }
+    }
+
+    function authorizeTo(address voter) public {
+        require(
+            msg.sender == Creator,
+            'Only the contract creator can authorize some people to vote'
+        );
+        require(Voters[voter].weight == 0, 'the current voter had been authorized');
+        Voters[voter].weight = 1;
+    }
+
+    function delegate(address to) public {
+        Voter storage sender = Voters[msg.sender];
+        require(!sender.voted, 'You have voted');
+        require(to != msg.sender, 'Self-delegate is not supported');
+
+        while(Voters[to].delegate != address(0)){
+            to = Voters[to].delegate;
+            
+        }
     }
 }
